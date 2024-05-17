1ae8d5103956051c2ed6256bc4329a40dd5c87ce
Vasileios Pasialiokis <whiterocket@outlook.com>
2024-05-17 03:15:40 +0300

    varkin tapless

--- varkin/keymap.c
+++ varkin/keymap.c
@@ -3,22 +3,32 @@
 enum LayerName {
 	_BASE  = 0,
 	_RAISE = 1,
+	_LOWER = 2,
+	_SYS   = 4,
 };
 
-/*
-g c d m f b w u o p
-r s t n h y i e a l
-*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT_ortho_3x10(
 		KC_G   , KC_C   , KC_D   , KC_M   , KC_F   , KC_B   , KC_W   , KC_U   , KC_O   , KC_P   ,
 		KC_R   , KC_S   , KC_T   , KC_N   , KC_H   , KC_Y   , KC_I   , KC_E   , KC_A   , KC_L   ,
-		KC_LCTL, KC_LGUI, KC_LALT, MO(1)  , KC_SPC , KC_LSFT, MO(1)  , KC_BTN1, KC_BTN2, KC_BTN3
+		KC_LGUI, KC_LALT, MO(2)  , MO(1)  , KC_SPC , KC_LCTL, KC_LSFT, KC_RCTL, KC_RALT, KC_RGUI
 	),
 
 	[_RAISE] = LAYOUT_ortho_3x10( // alpha 2 layer
-		KC_COMM, KC_Q   , KC_K   , KC_J   , KC_DOT , _______, KC_SCLN, KC_GRV , _______, _______,
-		KC_X   , KC_Z   , KC_V   , KC_QUOT, KC_MINS, KC_EQL , KC_RBRC, KC_LBRC, KC_SLSH, KC_BSLS,
+		KC_GRV , KC_Q   , KC_K   , KC_J   , KC_EQL , KC_BSLS, KC_COMM, KC_DOT , KC_RBRC, KC_LBRC,
+		KC_X   , KC_Z   , KC_V   , KC_QUOT, KC_MINS, KC_SLSH, KC_ENT , KC_TAB , KC_BSPC, KC_SCLN,
+		_______, _______, MO(3)  , _______, _______, _______, _______, _______, _______, _______
+	),
+
+	[_LOWER] = LAYOUT_ortho_3x10(
+		KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,
+		KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_DEL , KC_ESC , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,
+		_______, _______, _______, MO(3)  , _______, _______, _______, _______, _______, _______
+	),
+
+	[_SYS] = LAYOUT_ortho_3x10(
+		KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,
+		KC_F11 , KC_F12 , KC_INS , KC_CAPS, KC_PAUS, KC_PSCR, KC_APP , KC_VOLD, KC_VOLU, KC_MUTE
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT
 	),
 };
